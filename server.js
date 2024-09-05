55ee69b204e4a0dc7a18a8281fae79cf9175fe78
Kavita <thetekavita77@gmail.com>
2024-09-06 01:10:30 +0530

    build

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const io = new Server(server,{
 });
 
 app.use(express.static('build'));
+//if we only use this we get error after refresh in build
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
